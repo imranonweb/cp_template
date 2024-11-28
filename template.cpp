62e84254af670024aae60b7a72f155ce27d7b5e4
Al Imran Emon <138112368+imranonweb@users.noreply.github.com>
2024-11-28 21:31:58 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define nl "\n"
+#define io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int32_t main() {
+    io();  
+    int T;
+    cin >> T;  
+    while (T--) {
+        // Code Logic
+    }
+    return 0;
+}
