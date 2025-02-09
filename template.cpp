793ddf9c6b5cc9205d7a33d62d536989a828028f
Al Imran Emon <138112368+imranonweb@users.noreply.github.com>
2025-02-09 20:10:51 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,15 +1,39 @@
+//------ imranonweb Template ------//
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long int
 #define nl "\n"
 #define io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define vi vector<int>
+#define pii pair<int, int>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define inf 1e18
+#define mod 1000000007
+#define PI 3.1415926535897932384626
+#define MAX 1000005
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define EPSILON 1e-9
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+// __int128 for integers with more than 18 digits (e.g., 10^36)
 
+void solve () {
+    // Code Logic
+}
 int32_t main() {
     io();  
     int T;
     cin >> T;  
     while (T--) {
-        // Code Logic
+        solve();
     }
     return 0;
 }
