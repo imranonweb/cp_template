33d2636ad9f7ed067a607eae1de87af5e33e0e3d
Al Imran Emon <138112368+imranonweb@users.noreply.github.com>
2025-02-13 09:30:58 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long int
+#define ll long long
 #define nl "\n"
 #define io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define all(x) x.begin(), x.end()
 #define pb push_back
-#define mp make_pair
 #define vi vector<int>
 #define inf 1e18
 #define mod 1000000007
@@ -13,14 +12,14 @@ using namespace std;
 #define MAX 1000005
 #define gcd(a, b) __gcd(a, b)
 #define lcm(a, b) (a * b) / gcd(a, b)
-#define EPSILON 1e-9
 #define yes cout << "YES" << nl
 #define no cout << "NO" << nl
 // __int128 for integers with more than 18 digits (e.g., 10^36)
+
 void solve () {
-    // Code Logic
+    
 }
-int32_t main() {
+int main() {
     io();  
     int T;
     cin >> T;  
