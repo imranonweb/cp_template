9c78f58b0b5c15e65a5b1fd287cffe2dd67de9f1
Al Imran Emon <138112368+imranonweb@users.noreply.github.com>
2025-02-15 20:00:02 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,19 +3,9 @@ using namespace std;
 #define ll long long
 #define nl "\n"
 #define io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define all(x) x.begin(), x.end()
-#define pb push_back
-#define vi vector<int>
-#define inf 1e18
-#define mod 1000000007
-#define PI 3.1415926535897932384626
-#define MAX 1000005
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a * b) / gcd(a, b)
 #define yes cout << "YES" << nl
 #define no cout << "NO" << nl
-// __int128 for integers with more than 18 digits (e.g., 10^36)
-
+#define prec(n) cout << fixed << setprecision(n)
 void solve () {
     
 }
