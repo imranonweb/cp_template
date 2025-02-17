c4e925aef634f60163e3c7da9818f3e112b00e01
Al Imran Emon <138112368+imranonweb@users.noreply.github.com>
2025-02-17 23:12:06 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,10 +5,19 @@ using namespace std;
 #define io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define yes cout << "YES" << nl
 #define no cout << "NO" << nl
-#define prec(n) cout << fixed << setprecision(n)
-void solve () {
+#define setpr(n) cout << fixed << setprecision(n)
+#define vll vector<long long> 
+#define all(x) x.begin(), x.end()  
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define MOD 1000000007
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+
+void solve() {
     
-}
+  }
+
 int main() {
     io();  
     int T;
