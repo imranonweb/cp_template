0da74fac3535e9af28103474df4f18e69b5093eb
Al Imran Emon <138112368+imranonweb@users.noreply.github.com>
2025-03-10 00:17:46 +0600

    Create template1.cpp

---
+++ template1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl "\n"
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define setpr(n) cout << fixed << setprecision(n)
+#define vll vector<long long> 
+#define all(x) x.begin(), x.end()  
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define MOD 1000000007
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+void solve(){
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T;
+    cin >> T;  
+    while (T--)
+    solve();
+    return 0;
+}
