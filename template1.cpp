97fa9a8d3f530e9fa768350b77438c4065a15b0c
Al Imran Emon <138112368+imranonweb@users.noreply.github.com>
2025-05-09 12:01:14 +0600

    Update template1.cpp

--- template1.cpp
+++ template1.cpp
@@ -18,9 +18,9 @@ void solve(){
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int T;
-    cin >> T;  
-    while (T--)
+    int tc;
+    cin >> tc;  
+    while (tc--)
     solve();
     return 0;
 }
